49c7267b74a5a2359ad0fad8effc5286f465aeca
Efraim Filho <efraimleite@gmail.com>
2019-03-18 21:38:42 -0300

    Criação das classes Java para o projeto;

---
+++ app/src/main/java/com/example/joao_bovoloni/aps2/Caixa.java
@@ -0,0 +1,23 @@
+package com.example.joao_bovoloni.aps2;
+
+public class Caixa {
+
+    Boolean estaDormindo;
+    Boolean estaAtendendo;
+
+    public Boolean getEstaAtendendo() {
+        return estaAtendendo;
+    }
+
+    public Boolean getEstaDormindo() {
+        return estaDormindo;
+    }
+
+    public void setEstaAtendendo(Boolean estaAtendendo) {
+        this.estaAtendendo = estaAtendendo;
+    }
+
+    public void setEstaDormindo(Boolean estaDormindo) {
+        this.estaDormindo = estaDormindo;
+    }
+}
---
+++ app/src/main/java/com/example/joao_bovoloni/aps2/Gerenciamento.java
@@ -0,0 +1,12 @@
+package com.example.joao_bovoloni.aps2;
+
+import java.util.ArrayList;
+
+public class Gerenciamento {
+
+    Caixa caixa;
+    ArrayList<Cliente> fila;
+
+
+
+}
